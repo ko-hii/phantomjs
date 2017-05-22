422992f77c4dc4fd2d7d28973bb463e4a7a01137
ko-hii <hirose@cysec.cs.ritsumei.ac.jp>
2017-05-22 23:54:47 +0900

    Add files via upload

---
+++ phantom_get.py
@@ -0,0 +1,23 @@
+﻿from threading import Thread
+
+
+"""
+PhantomJSはseleniumのタイムアウト設定がきかないみたいなので、スレッド化してタイムアウトを実装する
+使い方は、呼び出し元でこのスレッドをstartしたあとにjoin(timeout=x)
+x秒joinで待った後、self.reの値を見て、状況を把握する
+戻り値(self.re)がいらないなら、クラスじゃなくて関数でもいい
+"""
+class PhantomGetThread(Thread):
+    def __init__(self, phantom_driver, url):
+        super(GetPhantomThread, self).__init__()
+        self.phantom_driver = phantom_driver
+        self.url = url
+        self.re = False
+
+    def run(self):
+        try:
+            self.phantom_driver.get(self.url)
+        except Exception as e:
+            self.re = e
+        else:
+            self.re = True
